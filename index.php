b2c448b33ca5ad6eb9b2af85c0d4457235d63fe4
Shelby Klein <shelbyklein@Shelbys-MacBook-Pro.local>
2025-05-06 13:15:35 -0400

    Update page title to be more specific

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>USAA Certificate Generator</title>
+    <title>USA Archery Certificate Generator</title>
     <style>
         body {
             font-family: Arial, sans-serif;
