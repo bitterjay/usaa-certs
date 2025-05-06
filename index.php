01643627bd0981605c3a7f7b61ee70765b961973
Shelby Klein <shelbyklein@Shelbys-MacBook-Pro.local>
2025-05-06 14:58:06 -0400

    Update version to 1.4.2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Define application version
-$app_version = "1.4.1";
+$app_version = "1.4.2";
 
 // Initialize position values in session if not set
 if (!isset($_SESSION['name_y_pos'])) $_SESSION['name_y_pos'] = 50;
