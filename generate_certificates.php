7997357fe2dea60be39eb3e285309586333c8638
Shelby Klein <shelbyklein@Shelbys-MacBook-Pro.local>
2025-05-06 20:29:48 -0400

    bg

--- generate_certificates.php
+++ generate_certificates.php
@@ -104,18 +104,18 @@ try {
         body { 
             margin: 0; 
             padding: 0;
-            width: 279.4mm;
-            height: 215.9mm;
+            width: 100%;
+            height: 100%;
             position: relative;
         }
         .bg { 
             position: absolute; 
             left: 0; 
             top: 0; 
-            width: 279.4mm; 
-            height: 215.9mm; 
+            width: 100%; 
+            height: 100%; 
             z-index: 0; 
-            object-fit: cover;
+            object-fit: fill;
             display: block;
         }
         .name {
